db14f53c71df28d745ed79d1f9582b13e2d5e2aa
Krishna Tholudur <krishna.tholudur@gmail.com>
2026-03-23 23:30:25 -0700

    finished collision tuning

--- Assets/BikeCollisionDetector.cs
+++ Assets/BikeCollisionDetector.cs
@@ -7,12 +7,20 @@ public class BikeCollisionDetector : MonoBehaviour
     public MotorcycleInput   input;
     public float crashSpeedThreshold = 5f;
 
+    public BoxCollider box;
+
     void OnCollisionEnter(Collision col)
     {
         if (physics == null || physics.IsCrashed) return;
 
         if (Mathf.Abs(physics.CurrentSpeed) > crashSpeedThreshold)
         {
+            if (box != null)
+            {
+                box.center = new Vector3(box.center.x, 0.61f, box.center.z);
+                box.size   = new Vector3(box.size.x,   1.22f, box.size.z);
+            }
+
             physics.TriggerCrash(physics.GetComponent<Rigidbody>().linearVelocity);
         }
     }
